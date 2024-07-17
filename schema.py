3188e16a8fa81d6c336cfa698f6149c2e26f8e48
Abhraneel Dey <abhradey.2311@gmail.com>
2024-07-17 14:40:57 +0530

    Updated Schema

--- schema.py
+++ schema.py
@@ -1,38 +1,39 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import User as UserModel, session
+from models import Students as StudentModel, session
 
-class User(SQLAlchemyObjectType):
+class Students(SQLAlchemyObjectType):
     class Meta:
-        model = UserModel
+        model = StudentModel
 
 class Query(graphene.ObjectType):
-    all_users = SQLAlchemyConnectionField(User.connection)
-    user = graphene.Field(User, id=graphene.Int())
-
-    def resolve_all_users(self, info):
-        query = User.get_query(info)
+    all_students = SQLAlchemyConnectionField(Students.connection)
+    student = graphene.Field(Students, id=graphene.Int())
+    
+    def resolve_all_students(self, info):
+        query = Students.get_query(info)
         return query.all()
- 
+    
     def resolve_user(self, info, id):
-        query = User.get_query(info)
+        query = Students.get_query(info)
         return query.get(id)
 
-class CreateUser(graphene.Mutation):
+class CreateStudent(graphene.Mutation):
     class Arguments:
         name = graphene.String()
         age = graphene.Int()
-        email = graphene.String()
-
-    user = graphene.Field(lambda: User)
-
-    def mutate(self, info, name, age, email):
-        user = UserModel(name=name, age=age, email=email)
-        session.add(user)
+        emailid = graphene.String()
+        contact = graphene.Int()
+    
+    student = graphene.Field(lambda: Students)
+    
+    def mutate(self, info, name, age, emailid, contact):
+        student = StudentModel(name=name, age=age, emailid=emailid, contact=contact)
+        session.add(student)
         session.commit()
-        return CreateUser(user=user)
-
+        return CreateStudent(student=student)
+    
 class Mutation(graphene.ObjectType):
-    create_user = CreateUser.Field()
-
+    create_student = CreateStudent.Field()
+    
 schema = graphene.Schema(query=Query, mutation=Mutation)
