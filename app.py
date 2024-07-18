3fedb34a29c912dba4c227e6dbfe7767c18f5be7
Abhraneel Dey <abhradey.2311@gmail.com>
2024-07-18 12:58:56 +0530

    Added session management

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, request
-from graphene import Schema
 from flask_graphql import GraphQLView
 from schema import schema
+from models import session
 
 app = Flask(__name__)
+app.debug=True
+
+def context_factory():
+    return {'session': session}
 
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view(
         'graphql',
         schema=schema,
-        graphiql=True  # Enable the GraphiQL UI
+        graphiql=True,  # Enable the GraphiQL UI
+        context_value = context_factory,
     )
 )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
